f379f88fd00f0bf7fd8907ddb3ae6af65277069e
Kermit <xkai08@qq.com>
2019-06-20 22:18:05 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,6 +3,7 @@
 if [ ! -f "/v2ray/config.json" ];then
   TOKEN=$(cat /proc/sys/kernel/random/uuid)
   echo "token is "$TOKEN
+  echo "alterId is 64"
   sed -e "s/##token##/${TOKEN}/" /v2ray/config-temp.json > /v2ray/config.json
 fi
 /v2ray/v2ray -config /v2ray/config.json
