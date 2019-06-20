856a62e9cb189c345e3727b047c7dcd133afdd1f
Kermit <xkai08@qq.com>
2019-06-20 22:16:23 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-TOKEN=$(cat /proc/sys/kernel/random/uuid)
-echo "token is "$TOKEN
-sed -e "s/##token##/${TOKEN}/" /v2ray/config-temp.json > /v2ray/config.json
+if [ ! -f "/v2ray/config.json" ];then
+  TOKEN=$(cat /proc/sys/kernel/random/uuid)
+  echo "token is "$TOKEN
+  sed -e "s/##token##/${TOKEN}/" /v2ray/config-temp.json > /v2ray/config.json
+fi
 /v2ray/v2ray -config /v2ray/config.json
